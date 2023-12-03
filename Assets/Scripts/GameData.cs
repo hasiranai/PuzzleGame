a970f7f4b7d7a14c1c444ecb8d4b5c41fbaed856
河内真輝 <>
2023-12-04 02:37:50 +0900

    今回...になってたもの

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -6,15 +6,15 @@ public class GameData : MonoBehaviour
 {
     public static GameData instance;
 
-    [Header("ゲームに登場する干支の最大登場数")]
+    [Header("ゲームに登場する干支の最大種類数")]
     public int etoTypeCount = 5;
 
     [Header("ゲーム開始時に生成する干支の数")]
     public int createEtoCount = 50;
 
-    private void Awake()
+    void Awake()
     {
-        if (instance = null)
+        if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,11 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField, Header("干支のプレファブ")]
-    private GameObject etoPlefab;
+    private Eto etoPrefab; // <= ⭐︎ 宣言する型を GameObject型 から Eto型に変更。EtoPrefabは前と同じようにProject内からアサインできる
 
     [SerializeField, Header("干支の生成位置")]
     private Transform etoSetTran;
@@ -19,10 +19,40 @@ public class GameManager : MonoBehaviour
     [SerializeField, Header("干支生成時の落下位置")]
     private float fallPos = 1400.0f;
 
-    private void Start()
+    [SerializeField, Header("生成された干支のリスト")]
+    private List<Eto> etoList = new List<Eto>();
+
+    [SerializeField, Header("干支の画像データ")]
+    private Sprite[] etoSprites;
+
+
+    IEnumerator Start()　　// <= ⭐︎ 戻り値を void から IEnumerator型に変更して、コルーチンメソッドにする
     {
+        // 干支の画像を読み込む。この処理が終了するまで、次の処理には行かないようにする
+        yield return StartCoroutine(LoadEtoSprites());
+
         // 引数で指定した数の干支を生成する
         StartCoroutine(CreateEtos(GameData.instance.createEtoCount));
+    }
+
+    /// <summary>
+    /// 干支の画像を読み込んで配列から使用できるようにする
+    /// </summary>
+    private IEnumerator LoadEtoSprites()
+    {
+        // 配列の初期化(12個の画像が入るようにSprite型の配列を12個用意する)
+        etoSprites = new Sprite[(int)EtoType.Count];
+
+        // Resources.LoadAllを行い、分割されている干支の画像を順番に全て読み込んで配列に代入
+        etoSprites = Resources.LoadAll<Sprite>("Sprites/eto");
+
+        // ※ 1つのファイルを１２分割していない場合は、以下の処理を行います。12分割している場合には使用しません。
+        //for(int i = 0; i < etoSprites.Length; i++)
+        //{
+        //    etoSprites[i] = Resources.Load<Sprite>("Sprites/eto_" + i);
+        //}
+
+        yield break;
     }
 
     /// <summary>
@@ -35,13 +65,22 @@ public class GameManager : MonoBehaviour
         for (int i = 0; i < generateCount; i++)
         {
             // 干支プレファブのクローンを、干支の生成位置に生成
-            GameObject eto = Instantiate(etoPlefab, etoSetTran, false);
+            Eto eto = Instantiate(etoPrefab, etoSetTran, false);  // <= ⭐︎ 変更。　生成された干支を代入する型をGameObject型からEto型に変更する
 
             // 生成された干支の回転情報を設定(色々な角度になるように)
             eto.transform.rotation = Quaternion.AngleAxis(Random.Range(-maxRotateAngle, maxRotateAngle), Vector3.forward);
 
             // 生成位置をランダムにして落下位置を変化させる
             eto.transform.localPosition = new Vector2(Random.Range(-maxRange, maxRange), fallPos);
+
+            // ランダムな干支を12種類の中から１つ選択
+            int randomValue = Random.Range(0, (int)EtoType.Count);
+
+            // 生成された干支の初期設定(干支の種類と干支の画像を引数を使ってEtoへ渡す)
+            eto.SetUpEto((EtoType)randomValue, etoSprites[randomValue]);
+
+            // etoListに追加
+            etoList.Add(eto);
 
             // 0.03秒待って次の干支を生成
             yield return new WaitForSeconds(0.03f);
