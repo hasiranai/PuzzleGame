b98e7e5aae5d6acad060938055b6cdbebefc69a9
河内真輝 <>
2024-01-29 03:22:02 +0900

    発展１５

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -13,15 +13,66 @@ public class Bomb : MonoBehaviour
     [SerializeField]
     private bool onGizmos = true;
 
+    public void SetUpBomb(GameManager gameManager, float bombRadius)
+    {
+        if (TryGetComponent(out btnBomb))
+        {
+            btnBomb.onClick.AddListener(() => OnClickBomb(gameManager, bombRadius));
+        }
+        else
+        {
+            Debug.Log("ボムのボタンが取得できません");
+        }
+
+        // OnDrawGizmos 用
+        radius = bombRadius;
+    }
+
+    /// <summary>
+    /// ボムをタップした際の処理
+    /// </summary>
+    public void OnClickBomb(GameManager gameManager, float bombRadius)
+    {
+        // TODO SE
+
+        List<Eto> eraseEtos = new List<Eto>();
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, bombRadius);
+
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.TryGetComponent(out Eto eto))
+            {
+                eraseEtos.Add(eto);
+            }
+        }
+
+        // TODO LINQ を利用した場合
 
 
 
+        // TODO GameManager への干支の削除命令を書く
 
 
+        Debug.Log("ボム実行");
 
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!onGizmos)
+        {
+            return;
+        }
+
+        // コライダーの可視化
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
 
     void Start()
     {
-        
+        // デバッグ用のテスト処理
+        SetUpBomb(null, 1.0f);
     }
 }
